8c3d0e7f2c525ec061bc28d14b883dab9e9a3d0f
MéLoBeats <lamelobeats@gmail.com>
2024-04-26 22:52:01 +0200

    feat: add project def

--- lib/screens/ar_screen.dart
+++ lib/screens/ar_screen.dart
@@ -1,33 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/language_alert_dialog.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ARScreen extends StatelessWidget {
-  // Return page with AR en cours de développement
-
-  const ARScreen({Key? key}) : super(key: key);
+class ARScreen extends StatefulWidget {
+  const ARScreen({super.key});
 
   static const routeName = '/ar';
 
   @override
+  ARScreenState createState() => ARScreenState();
+}
+
+class ARScreenState extends State<ARScreen> {
+  final List<AssetImage> imageList = [
+    const AssetImage('assets/images/Frame 10.png'),
+    const AssetImage('assets/images/Frame 11.png'),
+    const AssetImage('assets/images/Frame 12.png'),
+    const AssetImage('assets/images/Frame 13.png'),
+    const AssetImage('assets/images/Frame 14.png'),
+    const AssetImage('assets/images/Frame 15.png'),
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff3e4645),
       appBar: AppBar(
-        title: const Text('AR'),
-      ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(height: 60),
-            SizedBox(height: 50),
-            Text(
-              'AR en cours de développement',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+        centerTitle: true,
+        backgroundColor: const Color(0xff3e4645),
+        actions: <Widget>[
+          GFIconButton(
+            icon: const Icon(
+              Icons.language,
+              color: Colors.white,
             ),
-          ],
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const LanguageAlertDialog();
+                },
+              );
+            },
+            type: GFButtonType.transparent,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          GFCarousel(
+            enableInfiniteScroll: true,
+            activeIndicator: Colors.white,
+            autoPlay: true,
+            items: imageList.map(
+              (image) {
+                return Container(
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Image(
+                      image: image,
+                      fit: BoxFit.contain,
+                      width: 1000.0,
+                      height: 1000.0,
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+            onPageChanged: (index) {
+              setState(() {
+                index;
+              });
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: SizedBox(
+        width: 70,
+        height: 70,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/scanner');
+          },
+          backgroundColor: Colors.white,
+          child: const Icon(
+            Icons.qr_code,
+            size: 40.0,
+          ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
     );
   }
 }
